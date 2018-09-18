42cadddb311b8ddd41515bee5f39b2bcf91514fe
帅纯亮 <shuaichunliang@heqiauto.com>
2018-09-19 01:08:37 +0800

    single file upload test

---
+++ src/main/java/com/example/demo/web/UploadController.java
@@ -0,0 +1,46 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+@Controller
+@RequestMapping("/upload")
+public class UploadController {
+
+    @RequestMapping(value = "/upload-form",method = RequestMethod.GET)
+    public String uploadForm(Model model){
+        return "upload";
+    }
+
+    @RequestMapping(value = "/do-upload",method = RequestMethod.POST)
+    public String upload(@RequestParam MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                // 这里只是简单例子，文件直接输出到项目路径下。
+                // 实际项目中，文件需要输出到指定位置，需要在增加代码处理。
+                // 还有关于文件格式限制、文件大小限制。
+                BufferedOutputStream out = new BufferedOutputStream(
+                        new FileOutputStream(new File(file.getOriginalFilename())));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            }
+            return "上传成功";
+        } else {
+            return "上传失败，因为文件是空的.";
+        }
+    }
+}
---
+++ src/main/resources/templates/upload.ftl
@@ -0,0 +1,19 @@
+<html xmlns:th="http://www.thymeleaf.org">
+<body>
+
+	<h1>上传文件：</h1>
+
+	<div>
+		<form method="POST" enctype="multipart/form-data" action="/upload/do-upload">
+			<table>
+				<tr>
+				<td>上传文件:</td>
+				<td><input type="file" name="file" /></td>
+				</tr>
+				<tr><td></td><td><input type="submit" value="上传" /></td></tr>
+			</table>
+		</form>
+	</div>
+
+</body>
+</html>
